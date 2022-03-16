1b000d398ed776b9ff422528663f5f745eee49f7
QualityPre <qualitypre@protonmail.com>
2022-03-16 12:54:42 +0000

    added server file

--- app.js
+++ app.js
@@ -19,8 +19,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+module.exports = app;
