281053802144f09fcc7597a23d4c19fa224174c2
QualityPre <qualitypre@protonmail.com>
2022-03-17 16:24:12 +0000

    added a middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,15 @@ const tourController = require('../controllers/tourController');
 
 router.param('id', tourController.checkID);
 
+// create a checkbody middleware
+// check if body contains name and price property
+// if not send back 400 (bad request)
+// add it to the post handler stack (where middleware is)
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
