b896591b0312be6d3384d3f8a2b8c9135db3f852
QualityPre <qualitypre@protonmail.com>
2022-03-21 10:01:05 +0000

    mongodb

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 
 const morgan = require('morgan');
+
 const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // middleware
-app.use(morgan('dev'));
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello, this is middleware!');
+  next();
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connection) => console.log(connection.connections));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
